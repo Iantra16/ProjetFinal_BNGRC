eff9e2cc9b60ca392a216b914c1e067d7687bf7a
Moon4824 <salomonbovel@gmail.com>
2026-02-16 22:31:00 +0300

    donee

---
+++ databases/20260216-03.sql
@@ -0,0 +1,62 @@
+CREATE OR REPLACE VIEW v_reste_dons_disponibles AS
+SELECT 
+    da.id AS id_don_article,   
+    a.id AS id_article,
+    a.nom AS article,
+    -- Calcul direct du reste uniquement
+    (da.quantite - COALESCE((
+        SELECT SUM(dist.quantite_attribuee)
+        FROM distribution dist
+        WHERE dist.id_don_article = da.id
+    ), 0)) AS stock_restant,
+    a.unite
+FROM don_article da
+JOIN article a ON da.id_article = a.id
+-- On filtre pour ne voir que ce qui n'est pas encore épuisé
+HAVING stock_restant > 0;
+select * from v_reste_dons_disponibles;
+
+CREATE OR REPLACE VIEW v_reste_total_dons_disponibles AS
+SELECT 
+    a.nom AS article,
+    SUM(da.quantite) AS total_initial,
+    -- Somme de toutes les distributions pour cet article
+    COALESCE(SUM((
+        SELECT SUM(dist.quantite_attribuee)
+        FROM distribution dist
+        WHERE dist.id_don_article = da.id
+    )), 0) AS total_distribue,
+    -- Stock total disponible au dépôt
+    (SUM(da.quantite) - COALESCE(SUM((
+        SELECT SUM(dist.quantite_attribuee)
+        FROM distribution dist
+        WHERE dist.id_don_article = da.id
+    )), 0)) AS stock_total_restant,
+    a.unite
+FROM don_article da
+JOIN article a ON da.id_article = a.id
+GROUP BY a.nom, a.unite
+HAVING stock_total_restant > 0
+ORDER BY a.nom ASC;
+
+
+CREATE OR REPLACE VIEW v_reste_besoin AS
+SELECT 
+    ba.id AS id_besoin_article,
+    v.nom AS ville,
+    a.id AS id_article,
+    a.nom AS article,
+    -- Calcul du reste à combler (Demande - Somme des distributions reçues)
+    (ba.quantite - COALESCE((
+        SELECT SUM(dist.quantite_attribuee)
+        FROM distribution dist
+        WHERE dist.id_besoin_article = ba.id
+    ), 0)) AS reste_a_combler,
+    a.unite
+FROM besoin_article ba
+JOIN besoin b ON ba.id_besoin = b.id
+JOIN ville v ON b.id_ville = v.id
+JOIN article a ON ba.id_article = a.id
+-- Filtre pour ne garder que les besoins non terminés
+HAVING reste_a_combler > 0;
+
