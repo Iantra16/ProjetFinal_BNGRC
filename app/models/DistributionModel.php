19992aae9a4d4310556ac6abd56ed5ee770bd093
Moon4824 <salomonbovel@gmail.com>
2026-02-16 21:51:02 +0300

    init distribution

---
+++ app/models/DistributionModel.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace app\models;
+
+use Flight;
+
+class DistributionModel
+{
+    private $db;
+
+    public function __construct($db)
+    {
+        $this->db = $db;
+    }
+
+    function DistributoinDons($dons_ville_detaille) { 
+        
+    }
+
+}
